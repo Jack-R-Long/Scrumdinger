eec9789181b0d99dc844bdf156476b14e54e2cdc
Jack Long <jack.richard.long@gmail.com>
2023-08-20 13:50:04 -0500

    Initial Commit

---
+++ my_scrumdinger/my_scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_scrumdingerApp.swift
+//  my_scrumdinger
+//
+//  Created by Jack long on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct my_scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
